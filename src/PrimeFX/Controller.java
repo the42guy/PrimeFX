3bb365071c2ef16a1d35be837d41046b4d540062
the42guy <peter@alternateuniverse.cu.cc>
2015-07-19 18:39:28 +0530

    Updated the FXML

--- src/PrimeFX/Controller.java
+++ src/PrimeFX/Controller.java
@@ -1,14 +1,9 @@
 package PrimeFX;
 
-import javafx.application.Platform;
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
-
-import java.util.concurrent.CountDownLatch;
 
 
 public class Controller {
@@ -17,7 +12,7 @@ public class Controller {
     @FXML
     private TextField num;
     @FXML
-    private int rangeMin, rangeMax;
+    private int rangeMin, rangeMax, greatestIntFactor = 1;
     @FXML
     private TextField rangeStart, rangeEnd;
     @FXML
@@ -38,7 +33,7 @@ public class Controller {
         String numString = num.getText();
         numToCheck = intParser(numString);
         String isPrimeStr = checkIntPassOne(numToCheck) + "";
-        primeLabel.setText("The number " + numToCheck + " is prime: " + isPrimeStr);
+        primeLabel.setText("The number " + numToCheck + " is prime: " + isPrimeStr + "\nThe first integer dividing it is " + greatestIntFactor);
     }
 
     @FXML
@@ -47,18 +42,32 @@ public class Controller {
         boolean isPrime = false;
         if ((intToCheck == 2) || (intToCheck == 3)) {
             isPrime = true;
-        } else if ((intToCheck <= 1) || (intToCheck % 2 == 0)) {
+        } else if (intToCheck <= 1) {
             isPrime = false;
+
+        } else if (intToCheck % 2 == 0){
+            isPrime = false;
+            greatestIntFactor = 2;
         } else if (intToCheck > 3) {
             for(int n = 1; n < intToCheck; n++) {
                 progressIndicatorOne.setProgress(n);
-                if ((intToCheck == (6 * n) + 1) || (intToCheck == (6 * n) -1)) {
+                if ((intToCheck == (6 * n) + 1) || (intToCheck == (6 * n) - 1)) {
                     isPrime = checkIntPassTwo(intToCheck);
+                    setGreatestIntFactor(isPrime);
                     break;
+                } else {
+                    greatestIntFactor = n;
                 }
             }
         }
         return isPrime;
+    }
+
+    @FXML
+    private void setGreatestIntFactor(boolean isPrime) {
+        if(isPrime) {
+            greatestIntFactor = 1;
+        }
     }
 
     @FXML
@@ -67,6 +76,8 @@ public class Controller {
         for (int multiplier = 3; multiplier < passTwoInt/2; multiplier = multiplier + 2) {
             if (passTwoInt % multiplier == 0) {
                 isPrimePassTwo = false;
+                greatestIntFactor = multiplier;
+                break;
             }
         }
         return isPrimePassTwo;
